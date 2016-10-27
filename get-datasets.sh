7f93e0cc413b446a2872e85efcd28572cf35e553
RalfMayet <trilly@gmail.com>
2016-10-27 21:46:32 +0800

    added mnist test script

--- get-datasets.sh
+++ get-datasets.sh
@@ -3,7 +3,6 @@
 mkdir datasets
 cd datasets
 wget http://deeplearning.net/data/mnist/mnist.pkl.gz
-gunzip mnist.pkl.gz
 wget https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
 tar xzvf cifar-10-python.tar.gz
 cd ..
